c37bbef44c0436dcebd7b5e975c3ff73db7a7967
Prajjwal Goswami <77458269+kinston18@users.noreply.github.com>
2022-06-17 20:31:14 +0530

    Add files via upload

---
+++ JavaScript/if.js
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>Starting JavaScript if statement...</title>
+    </head>
+    <body>
+        <script type="text/javascript">
+            <!--
+                var x = -11;
+                if (x=0)
+                {document.write("<h1><i>No. is positive!</i></h1>");}
+                if(x==0)
+                {document.write("No. is zero!");}
+                if (x<0)
+                {document.write("<h1><u>No. is negative!</u></h1><br>");}
+            //-->
+</script>
+    </body>
+</html>
